9903204b51b9817c72dccfeadc4a2439a159dc60
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-10-22 18:51:40 +0300

    Face ID used

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblInfo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func SigninClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error : NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is is you?") { success, error in
+                if success == true{
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                }else{
+                    DispatchQueue.main.async {
+                        self.lblInfo.text = "Error!"
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
